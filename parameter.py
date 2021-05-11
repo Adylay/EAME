a6515798350b00610bf23beeada764080cb1d3b6
adylay <ewe.drelich@gmail.com>
2021-05-11 12:42:50 +0200

    peak detector

---
+++ parameter.py
@@ -0,0 +1,14 @@
+filter_params = {
+    'signal_frequency': 255,
+    'filter_lowcut': 0.1,
+    'filter_highcut': 15.0,
+    'filter_order': 2
+}
+convolve_params = {
+    'integration_window': 15
+}
+
+detection_params = {
+    'findpeaks_limit': 0.35,
+    'findpeaks_spacing': 50
+}
